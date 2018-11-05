dc89d406e0832adcbcce5f6fc828a70a7370ad87
njuelle <n.juelle@gmail.com>
2018-11-05 08:15:32 +0100

    typos

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,6 +1,6 @@
 package arrays
 
-// Sum return the sum of five
+// Sum return the sum of
 // integers contained in an array
 func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
@@ -9,7 +9,8 @@ func Sum(numbers []int) (sum int) {
 	return
 }
 
-//SumAll return an array of sum of another array
+// SumAll return an array of integers sum
+// from anothers arrays
 func SumAll(numbersToSum ...[]int) (sums []int) {
 	return
 }
